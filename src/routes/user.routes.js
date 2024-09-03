ec15778f69245317133a50ff267c3039cb1c9b8b
tkdking00 <musama4288921@gmail.com>
2024-09-03 03:07:50 -0700

    add routs in router

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,25 +1,48 @@
-import {Router} from "express"
-import { loginUser, logoutUser, ragisterUser, refreshAccessToken } from "../controllers/user.controller.js"
-import upload from "../middlewares/multer.meddleware.js"
-import { varifyJWT } from "../middlewares/auth.middlewares.js"
+import { Router } from "express";
+import {
+  apdateAcountDetails,
+  apdateUserAvatar,
+  apdateUserCoverImage,
+  changeCurrentPassword,
+  getCurrentUser,
+  getUserChennleProfile,
+  getWatvchHistory,
+  loginUser,
+  logoutUser,
+  ragisterUser,
+  refreshAccessToken,
+} from "../controllers/user.controller.js";
+import upload from "../middlewares/multer.meddleware.js";
+import { varifyJWT } from "../middlewares/auth.middlewares.js";
 
-const router = Router()
+const router = Router();
 
 router.route("/register").post(
-    upload.fields([
+  upload.fields([
     {
-        name:"avatar",
-        maxCount:1
+      name: "avatar",
+      maxCount: 1,
     },
     {
-        name:"coverImage",// mn nay yaha pr coverImage field nahe add kia tha jo mn nay bad mn add kia 
-        maxCount:1
-    }
-])  ,
-ragisterUser)
-router.route("/login").post(loginUser)
+      name: "coverImage", // mn nay yaha pr coverImage field nahe add kia tha jo mn nay bad mn add kia
+      maxCount: 1,
+    },
+  ]),
+  ragisterUser
+);
+router.route("/login").post(loginUser);
 //secure routes
-router.route("/logout").post(  varifyJWT ,logoutUser )
-router.route("/refresh-token").post( refreshAccessToken )
-
-export default router
+router.route("/logout").post(varifyJWT, logoutUser);
+router.route("/refresh-token").post(refreshAccessToken);
+router.route("/change-password").post(varifyJWT, changeCurrentPassword);
+router.route("/current-user").get(varifyJWT, getCurrentUser);
+router.route("/update-account").patch(varifyJWT, apdateAcountDetails);
+router
+  .route("/avatar")
+  .patch(varifyJWT, upload.single("avatar"), apdateUserAvatar);
+router
+  .route("/cover-Image")
+  .patch(varifyJWT, upload.single("coverImage"), apdateUserCoverImage); // TODO: yaha pr /cover-Image likha h sir nay mn nay / nahe likha error hoa to dekh lo ga
+router.route("/c/:username").get(varifyJWT, getUserChennleProfile);
+router.route("/watchHistory").get(varifyJWT, getWatvchHistory);
+export default router;
